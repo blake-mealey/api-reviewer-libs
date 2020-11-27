c04b55f2b9f54917a075b7cca659aaef463a7a33
Blake Mealey <blakemealey@gmail.com>
2020-11-27 16:51:04 -0700

    Moved data loading to example

--- packages/api-reviewer-renderer/src/index.tsx
+++ packages/api-reviewer-renderer/src/index.tsx
@@ -1,120 +1,7 @@
 import * as React from 'react';
 
-import { convert } from 'api-reviewer-converter';
 import { IApiDocumentNode } from 'api-reviewer-converter/src/api-document/IApiDocumentNode';
-
-const data = `openapi: 3.0.0
-info:
-  version: 1.0.0
-  title: Swagger Petstore
-  license:
-    name: MIT
-servers:
-  - url: http://petstore.swagger.io/v1
-paths:
-  /pets:
-    get:
-      summary: List all pets
-      operationId: listPets
-      tags:
-        - pets
-      parameters:
-        - name: limit
-          in: query
-          description: How many items to return at one time (max 100)
-          required: false
-          schema:
-            type: integer
-            format: int32
-      responses:
-        200:
-          description: An paged array of pets
-          headers:
-            x-next:
-              description: A link to the next page of responses
-              schema:
-                type: string
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/Pets'
-        default:
-          description: unexpected error
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/Error'
-    post:
-      summary: Create a pet
-      operationId: createPets
-      tags:
-        - pets
-      responses:
-        201:
-          description: Null response
-        default:
-          description: unexpected error
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/Error'
-  /pets/{petId}:
-    get:
-      summary: Info for a specific pet
-      operationId: showPetById
-      tags:
-        - pets
-      parameters:
-        - name: petId
-          in: path
-          required: true
-          description: The id of the pet to retrieve
-          schema:
-            type: string
-      responses:
-        200:
-          description: Expected response to a valid request
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/Pets'
-        default:
-          description: unexpected error
-          content:
-            application/json:
-              schema:
-                $ref: '#/components/schemas/Error'
-components:
-  schemas:
-    Pet:
-      required:
-        - id
-        - name
-      properties:
-        id:
-          type: integer
-          format: int64
-        name:
-          type: string
-        tag:
-          type: string
-    Pets:
-      type: array
-      items:
-        $ref: '#/components/schemas/Pet'
-    Error:
-      required:
-        - code
-        - message
-      properties:
-        code:
-          type: integer
-          format: int32
-        message:
-          type: string
-`;
-
-const apiDocument = convert(data);
+import { IApiDocument } from 'api-reviewer-converter/dist/api-document/IApiDocument';
 
 interface NodeProps {
   node?: IApiDocumentNode;
@@ -132,7 +19,11 @@ function Node({ node, children }: React.PropsWithChildren<NodeProps>) {
   );
 }
 
-export const Thing = () => {
+interface ApiDocumentProps {
+  apiDocument: IApiDocument;
+}
+
+export const ApiDocument = ({ apiDocument }: ApiDocumentProps) => {
   return (
     <Node node={apiDocument.spec}>
       {apiDocument.spec?.name?.value}: {apiDocument.spec?.version?.value}
