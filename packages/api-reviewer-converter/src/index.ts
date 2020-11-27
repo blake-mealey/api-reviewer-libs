13129dba5d236ec74797900c38b58f5d76f63de7
Blake Mealey <blakemealey@gmail.com>
2020-11-26 22:10:36 -0700

    use converter in renderer

--- packages/api-reviewer-converter/src/index.ts
+++ packages/api-reviewer-converter/src/index.ts
@@ -1,8 +1,7 @@
 import { parseDocument } from 'yaml';
-import { read } from 'fs-jetpack';
 import { ApiDocumentParserFactory } from './parsers';
 
-function convert(data: string) {
+export function convert(data: string) {
   const document = parseDocument(data);
   // console.dir(document.contents, { depth: null });
 
@@ -10,9 +9,7 @@ function convert(data: string) {
   return parser.parse();
 }
 
-const data = read('petstore.yaml');
-const apiDocument = convert(data);
-
-console.dir(apiDocument, { depth: null });
-
-export default convert;
+// import { read } from 'fs-jetpack';
+// const data = read('petstore.yaml');
+// const apiDocument = convert(data);
+// console.dir(apiDocument, { depth: null });
